4fe987e6f06729b6c2ee5404de10f12e4fbd74e9
Max Qiu <maxzqiu@gmail.com>
2024-02-11 14:07:04 -0800

    Init

---
+++ src.js
@@ -0,0 +1,36 @@
+let Day=""
+let Hour=""
+let Min=""
+const REQUEST = document.querySelector("#request")
+const TIME = document.querySelector("#time")
+const BUTTON = document.querySelector("#button")
+const RESETTIME = document.querySelector("#lastreset")
+d=""
+function time(){    
+    d = new Date();
+    d=d.toString()
+    Day=d.substring(0,3);
+    Hour=d.substring(16,18);
+    Min=d.substring(19,21);
+}
+
+
+
+function checktime(){
+    time()
+    if (Hour===16 && Min===10 && Min===11 && Min===12 && Min===13 && Min===14 && Min===15){
+        REQUEST.textContent="Request Sent for Apple Berry Juice at "+Hour+":"+Min;
+        BUTTON.disabled="false"
+    }
+    TIME.textContent=d
+    
+}
+
+BUTTON.onclick = function (){
+    REQUEST.textContent="No Request"
+    BUTTON.disabled="true"
+    RESETTIME.textContent=new Date()
+}
+
+setInterval(checktime, 60000)
+
